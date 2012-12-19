27360249f0b04aa4adffe0d3e430dabc74aedb9e
alec <alecchyi@gmail.com>
2012-12-19 17:01:36 +0800

    project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchoolInfo::Application.config.secret_token = 'd558b9b98a96ab857f9561424955847947c018890fe54fb0abe2f5e77c5610defc8b9a370eb1720e70d33efc10a4a6f67ff15595cabbe0b364b28f1783ce7af9'
